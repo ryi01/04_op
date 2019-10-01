d84f858038d1868268a97fd198d3df8d875f3beb
ryi01 <55128336+ryi01@users.noreply.github.com>
2019-10-01 15:07:34 +0900

    04_op4

--- main.c
+++ main.c
@@ -5,7 +5,31 @@
 
 int main(int argc, char *argv[]) {
 	
-	int x, y; //practice3
+	/*practice1*/
+	int x, y, z, m;
+	int a, b, c; 
+	
+	x=1;
+	z=2;
+	a=3;
+	b=4;
+	c=5;
+	
+	y=m*x+b;
+	y=a*x*x+b*x+c;
+	m=(x+y+c)/3;
+	
+	/*practice2*/
+	int x=3; 
+	int cond1, cond2;
+	
+	cond1=2<x&&x<5;
+	cond2=x>1||x<4&&x>3;
+	
+	printf("cond1=%d, cond2=%d", cond1, cond2);
+	
+	/*practice3*/ 
+	int x, y; 
 
 	printf("enter two interge : ");
 	scanf("%d %d", &x, &y);
